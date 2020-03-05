f3dd19448afbbd14952b288d795aa56d4ad76dbf
savva-kotov <arlekin0011@gmail.com>
2020-03-05 21:05:33 +0300

    Create 1-1-6.py

---
+++ 1-1-6.py
@@ -0,0 +1,15 @@
+'''
+На вход подаются два числа x, y. 
+Умножьте 3 на x и запишите результат y раз подряд.
+
+Sample Input:
+3
+2
+
+Sample Output:
+99
+'''
+# put your python code here
+a, b = int(input()), int(input())
+print(str(3*a)*b)
+
