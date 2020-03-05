4451a3a166d4e9980323dbe9c67cfb7ad6f39111
savva-kotov <arlekin0011@gmail.com>
2020-03-05 21:00:24 +0300

    Create 1-1-2.py

---
+++ 1-1-2.py
@@ -0,0 +1,12 @@
+'''
+Напишите программу, которая напечатает букву "A" (латинскую) 133 раза подряд.
+Используйте смекалку, а не трудолюбие.
+Латинская A, а не русская А.  Обратите внимание на раскладку клавиатуры.
+
+Sample Input:
+
+Sample Output:
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+'''
+# put your python code here
+print('A'*133)
