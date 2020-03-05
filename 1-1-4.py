b9dd100295afff6130ae6a79c1ee2e4e2823e2ff
savva-kotov <arlekin0011@gmail.com>
2020-03-05 21:03:46 +0300

    Create 1-1-4.py

---
+++ 1-1-4.py
@@ -0,0 +1,13 @@
+'''
+Прочитайте строку, введенную пользователем. Напечатайте её три раза подряд.
+Подсказка: используйте функцию input.
+
+Sample Input:
+Python
+
+Sample Output:
+PythonPythonPython
+'''
+# put your python code here
+i = input()
+print(i*3)
