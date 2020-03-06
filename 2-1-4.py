89511a7f20b382cb9faf034c6d5007796063349e
savva-kotov <arlekin0011@gmail.com>
2020-03-06 14:09:07 +0300

    Create 2-1-4.py

---
+++ 2-1-4.py
@@ -0,0 +1,13 @@
+'''
+Считайте цифру, выведите её название на английском (zero, one, ..., nine).
+
+Sample Input:
+8
+
+Sample Output:
+eight
+'''
+a = int(input())
+l = ['zero','one','two','three','four','five','six','seven','eight','nine']
+print(l[a])
+
