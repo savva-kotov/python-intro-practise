89d9eaaeae26614bf80a8d68b7f8740d6f086dd6
savva-kotov <arlekin0011@gmail.com>
2020-03-11 21:45:08 +0300

    Create 3-1-9.py

---
+++ 3-1-9.py
@@ -0,0 +1,15 @@
+'''
+Прочитайте строку и сделайте из неё список.
+
+Sample Input:
+8 11
+
+Sample Output:
+['8', ' ', '1', '1']
+'''
+a = input()
+res = []
+for i in a:
+    res.append(i)
+print(res)
+
